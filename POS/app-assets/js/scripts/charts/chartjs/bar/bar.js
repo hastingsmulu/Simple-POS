4d29c148bb683a7f8b257848652d90d9bd5a4d93
Hastings Mumo Mulu <hastingsmumo@gmail.com>
2021-04-15 16:31:17 +0300

    Add files via upload

---
+++ POS/app-assets/js/scripts/charts/chartjs/bar/bar.js
@@ -0,0 +1,102 @@
+/*=========================================================================================
+    File Name: bar.js
+    Description: Chartjs bar chart
+    ----------------------------------------------------------------------------------------
+    Item Name: Robust - Responsive Admin Theme
+    Version: 1.2
+    Author: PIXINVENT
+    Author URL: http://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+
+// Bar chart
+// ------------------------------
+$(window).on("load", function(){
+
+    //Get the context of the Chart canvas element we want to select
+    var ctx = $("#bar-chart");
+
+    // Chart Options
+    var chartOptions = {
+        // Elements options apply to all of the options unless overridden in a dataset
+        // In this case, we are setting the border of each horizontal bar to be 2px wide and green
+        elements: {
+            rectangle: {
+                borderWidth: 2,
+                borderColor: 'rgb(0, 255, 0)',
+                borderSkipped: 'left'
+            }
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        responsiveAnimationDuration:500,
+        legend: {
+            position: 'top',
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }],
+            yAxes: [{
+                display: true,
+                gridLines: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                scaleLabel: {
+                    display: true,
+                }
+            }]
+        },
+        title: {
+            display: false,
+            text: 'Chart.js Horizontal Bar Chart'
+        }
+    };
+function showGraph(Cartegory)
+        {
+            {
+                $.post("datac.php",
+                function (chartdata)
+                {
+                    console.log(chartdata);
+                     var cartegory = [];
+                    var total = [];
+                    var status=[];
+
+                    for (var y in chartdata) {
+                        cartegory.push(chartdata[y].name);
+                        total.push(chartdata[y].qty);
+                    }
+
+                    var chartdata = {
+                        labels: cartegory,
+                        datasets: [
+                            {
+                                label: 'Quantity',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: total
+                            }
+                        ]
+                    };
+    var config = {
+        type: 'horizontalBar',
+
+        // Chart Options
+        options : chartOptions,
+
+        data : chartData
+    };
+
+    // Create the chart
+    var lineChart = new Chart(ctx, config);
+});
